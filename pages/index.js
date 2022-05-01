a2d11350b8317e77fa4aa61a82d27c0641aeb457
david <f312213213david@gmail.com>
2022-05-01 22:01:09 +0800

    0.9.5 Add home text

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ const Home = () => {
         <Meta title={'首頁 | 好用的縮網址'} description={'快來用我！'} />
         <div className={'flex justify-center items-center flex-col bg-indigo-200 space-y-3 w-2/3 h-96'}>
           <h1 className={'text-4xl'}>縮網址！</h1>
+          <p className={'text-2xl'}>此服務在登入後即可使用！</p>
         </div>
       </>
   )
