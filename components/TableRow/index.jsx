024b2d878abad754ddae2d7666903b7e9bef7043
david <f312213213david@gmail.com>
2022-05-15 02:21:02 +0800

    0.9.12 edit url display

--- components/TableRow/index.jsx
+++ components/TableRow/index.jsx
@@ -18,7 +18,7 @@ const TableRow = ({ clicks, created, customName, hashID, originalURL }) => {
         </td>
         <td className="table-cell py-4 w-2 px-6 text-sm font-medium text-gray-500  dark:text-white text-center max-w-10 overflow-hidden whitespace-nowrap text-ellipsis">
           <a href={originalURL} target={'_blank'} className="text-blue-600 dark:text-blue-500 hover:underline " rel="noreferrer">
-            {originalURL}
+            {originalURL.substr(8)}
           </a>
         </td>
         <td className="table-cell py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white text-center">
